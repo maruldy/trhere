c003f61f8dd636d5a7d1a9ff015246b083cd2f6b
maruldy <maruldy@gmail.com>
2019-02-17 23:10:39 +0900

    Created a superuser

--- main/models.py
+++ main/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Post(models.Model):
+    author_id = models.TextField()
+    author_name = models.TextField()
+    main_text = models.TextField()
+    image_path = models.TextField()
+    created_date = models.DateTimeField(
+            default=timezone.now)
+    published_date = models.DateTimeField(
+            blank=True, null=True)
+    
